4fe527ab6d015ede08f94c39c50bfe719190774c
ccostarod <ccostarodrigoo@gmail.com>
2024-03-12 10:21:01 -0300

    Implementações na classe controladora Agenda

--- desafios/desafios-unid1/desafio#1.2/controller/Agenda.js
+++ desafios/desafios-unid1/desafio#1.2/controller/Agenda.js
@@ -27,16 +27,45 @@ class Agenda {
     }
 
     excluirPaciente(cpf){
-        const pacienteExcluido = this.#listaPacientes.find(x => x.cpf === cpf);
-        if (pacienteExcluido){
+        if (this.#listaPacientes.some(x => x.cpf === cpf)){ 
             if (this.#listaConsultas.some(x => x.cpfPaciente === cpf)){
-                return false;
+                let consultasPassadas = [];
+                this.#listaConsultas.forEach(consulta => {
+                    let dataConsulta = consulta.dataConsulta.toISOString().split('T')[0];
+                    let dataAtual = new Date().toISOString().split('T')[0];
+                    if (consulta.cpfPaciente === cpf && dataConsulta > dataAtual){
+                        return false;
+                    }
+                    else if(consulta.cpfPaciente === cpf && dataConsulta < dataAtual){
+                        consultasPassadas.push(consulta);
+                    }
+                    else if(consulta.cpfPaciente === cpf && dataConsulta === dataAtual){
+                        horasConsulta = parseInt(consulta.horaInicial.substring(0,2));
+                        minutosConsulta = parseInt(consulta.horaInicial.substring(2));  
+                        horasAtual = new Date().getHours();
+                        minutosAtual = new Date().getMinutes();
+
+                        if (horasConsulta < horasAtual || (horasConsulta === horasAtual && minutosConsulta < minutosAtual)){
+                            consultasPassadas.push(consulta);
+                        }
+                        else 
+                            return false;
+                    }
+
+                });
+                this.#listaConsultas = this.#listaConsultas.filter(consulta => !consultasPassadas.includes(consulta));
             }
-            this.#listaPacientes = this.#listaPacientes.filter(x => x === pacienteExcluido);
+            this.#listaPacientes = this.#listaPacientes.filter(x => x.cpf !== cpf);
             return true;
         }
         return false;
     }
 
-    
+    acharPaciente(cpf){
+        return this.#listaPacientes.find(x => x.cpf === cpf);
+    }
+
+    agendarConsulta(Consulta){
+        
+    }
 }
---
+++ desafios/desafios-unid1/desafio#1.2/teste.js
@@ -0,0 +1,4 @@
+let horaInicial = '1400';
+let horas = horaInicial.substring(0,2);
+let minutos = horaInicial.substring(2);
+console.log(`${horas} horas e ${minutos} minutos!`)
