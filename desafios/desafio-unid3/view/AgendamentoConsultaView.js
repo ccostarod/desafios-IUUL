200a709bd772a068679f4487a9c33ca8185785c4
Rodrigo <ccostarodrigoo@gmail.com>
2024-04-20 22:30:51 -0300

    Criacao de helpers

---
+++ desafios/desafio-unid3/view/AgendamentoConsultaView.js
@@ -0,0 +1,10 @@
+import { DateTime } from "luxon";
+import { OperationErrors, OperationStatus } from "../controller/OperationCode"
+import { ValidaCPF } from "../model/Paciente"
+import Input from "../helpers/Input"
+import Output from "../helpers/output";
+
+
+class AgendamentoConsultaView {
+    
+}
